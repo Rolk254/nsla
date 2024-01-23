5959e5873fae03e4af2b96768f6f0be78d92f564
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-23 09:36:20 +0100

    wvww

--- scripts/temporadas.js
+++ scripts/temporadas.js
@@ -11,6 +11,10 @@ function redirigir() {
             console.log("Redirigiendo a calendar_t2.xml");
             window.location.href = "calendar_t2.xml";
             break;
+        case "3":
+            console.log("Redirigiendo a calendar_t2.xml");
+            window.location.href = "../index.html";
+            break;
         // Agrega más casos según sea necesario para otras temporadas
         default:
             console.error("Temporada no reconocida");
