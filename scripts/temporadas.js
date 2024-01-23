7375ff80547aebdca7c4c77f83fb79f28f7673b0
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-23 08:57:19 +0100

    ww

--- scripts/temporadas.js
+++ scripts/temporadas.js
@@ -1,16 +1,22 @@
 function redirigir() {
     var temporadaSeleccionada = document.getElementById("temporadaSelector").value;
 
+    // Muestra el valor seleccionado en la consola (para propósitos de depuración)
+    console.log("Temporada seleccionada:", temporadaSeleccionada);
+
     // Redirigir según la temporada seleccionada
     switch (temporadaSeleccionada) {
         case "1":
-            window.location.href = "../xm_xs/calendar_t1.xml";
+            console.log("Redirigiendo a calendar_t1.xml");
+            window.location.href = "calendar_t1.xml";
             break;
         case "2":
-            window.location.href = "../xm_xs/calendar_t2.xml";
+            console.log("Redirigiendo a calendar_t2.xml");
+            window.location.href = "calendar_t2.xml";
             break;
         // Agrega más casos según sea necesario para otras temporadas
         default:
+            console.error("Temporada no reconocida");
             break;
     }
 }
