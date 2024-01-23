fac57b0d5496922aa2a12fb5c504f90544ae6159
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-23 09:09:37 +0100

    wccw

--- scripts/temporadas.js
+++ scripts/temporadas.js
@@ -8,11 +8,11 @@ function redirigir() {
     switch (temporadaSeleccionada) {
         case "1":
             console.log("Redirigiendo a calendar_t1.xml");
-            window.location.href = "calendar_t1.xml";
+            window.location.href = "../xm_xs/calendar_t1.xml";
             break;
         case "2":
             console.log("Redirigiendo a calendar_t2.xml");
-            window.location.href = "calendar_t2.xml";
+            window.location.href = "../xm_xs/calendar_t2.xml";
             break;
         // Agrega más casos según sea necesario para otras temporadas
         default:
