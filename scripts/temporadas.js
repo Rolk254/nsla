625c32d06ac0730ed7a37da14d378066911aed23
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-23 09:21:50 +0100

    qdq

--- scripts/temporadas.js
+++ scripts/temporadas.js
@@ -1,22 +1,22 @@
 function redirigir() {
     var temporadaSeleccionada = document.getElementById("temporadaSelector").value;
 
-    // Muestra el valor seleccionado en la consola (para propósitos de depuración)
-    console.log("Temporada seleccionada:", temporadaSeleccionada);
-
     // Redirigir según la temporada seleccionada
     switch (temporadaSeleccionada) {
         case "1":
             console.log("Redirigiendo a calendar_t1.xml");
-            window.location.href = "../xm_xs/calendar_t1.xml";
+            window.location.href = "calendar_t1.xml";
             break;
         case "2":
             console.log("Redirigiendo a calendar_t2.xml");
-            window.location.href = "../xm_xs/calendar_t2.xml";
+            window.location.href = "calendar_t2.xml";
             break;
         // Agrega más casos según sea necesario para otras temporadas
         default:
             console.error("Temporada no reconocida");
             break;
     }
+
+    // Restablecer el valor del select para que el evento onchange se active nuevamente
+    document.getElementById("temporadaSelector").value = "0";  // Cambia "0" al valor predeterminado
 }
