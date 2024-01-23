178ec321423faa03e2f2ac3f86f51dd5011b2820
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-23 08:54:20 +0100

    qw

--- scripts/temporadas.js
+++ scripts/temporadas.js
@@ -4,10 +4,10 @@ function redirigir() {
     // Redirigir según la temporada seleccionada
     switch (temporadaSeleccionada) {
         case "1":
-            window.location.href = "calendar_t1.xml";
+            window.location.href = "../xm_xs/calendar_t1.xml";
             break;
         case "2":
-            window.location.href = "calendar_t2.xml";
+            window.location.href = "../xm_xs/calendar_t2.xml";
             break;
         // Agrega más casos según sea necesario para otras temporadas
         default:
