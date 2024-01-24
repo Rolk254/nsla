ab4b264f28166eaf9806b89dfb1662ac611bebbe
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-24 08:37:40 +0100

    FWFW

--- scripts/prueba.js
+++ scripts/prueba.js
@@ -3,7 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.getElementById('sidebar');
   
     menuToggle.addEventListener('click', function () {
-      if (sidebar.style.left === '-200px') {
+      const computedStyle = getComputedStyle(sidebar);
+      const sidebarLeft = parseInt(computedStyle.left);
+  
+      if (sidebarLeft === -200) {
         sidebar.style.left = '0';
       } else {
         sidebar.style.left = '-200px';
