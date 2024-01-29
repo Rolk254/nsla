0e54a3118c85814f65d0d21ff4a111c5c659de4e
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-29 09:44:25 +0100

    cww

--- scripts/clasificacion.js
+++ scripts/clasificacion.js
@@ -1,33 +1,37 @@
-// Cargar el XML
-var url = '../xm_xs/calendar_t1.xml';
-
-fetch(url)
-  .then(response => response.text())
-  .then(data => {
-    var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(data, 'application/xml');
-
-    // Llamar a la función para construir la tabla
-    construirTabla(xmlDoc);
-  })
-  .catch(error => console.error('Error al cargar el XML:', error));
-
-// Función para construir la tabla con los datos del XML
-function construirTabla(xml) {
-  // Obtener la referencia al cuerpo de la tabla
-  var tbody = document.getElementById('tablaBody');
-
-  // Obtener la lista de equipos del XML
-  var equipos = xml.querySelectorAll('equipo');
-
-  // Iterar sobre los equipos y construir las filas de la tabla
-  equipos.forEach(function(equipo) {
-    var nombre = equipo.querySelector('nombre').textContent;
-
-    // Crear una nueva fila
-    var fila = tbody.insertRow();
-
-    // Añadir celdas a la fila
-    fila.insertCell(0).textContent = nombre;
+document.addEventListener('DOMContentLoaded', function () {
+    // Resto del código aquí
+  
+    var url = '../xm_xs/calendar_t1.xml';
+  
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(data, 'application/xml');
+  
+        // Llamar a la función para construir la tabla
+        construirTabla(xmlDoc);
+      })
+      .catch(error => console.error('Error al cargar el XML:', error));
+  
+    // Función para construir la tabla con los datos del XML
+    function construirTabla(xml) {
+      // Obtener la referencia al cuerpo de la tabla
+      var tbody = document.getElementById('tablaBody');
+  
+      // Obtener la lista de equipos del XML
+      var equipos = xml.querySelectorAll('equipo');
+  
+      // Iterar sobre los equipos y construir las filas de la tabla
+      equipos.forEach(function(equipo) {
+        var nombre = equipo.querySelector('nombre').textContent;
+  
+        // Crear una nueva fila
+        var fila = tbody.insertRow();
+  
+        // Añadir celdas a la fila
+        fila.insertCell(0).textContent = nombre;
+      });
+    }
   });
-}
+  
