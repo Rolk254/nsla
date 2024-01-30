65b4f9a383d837866f62bcba5ff544d111b92fac
Rolk254 <113716271+Rolk254@users.noreply.github.com>
2024-01-30 12:29:05 +0100

    dq

--- scripts/clasificacion.js
+++ scripts/clasificacion.js
@@ -80,13 +80,7 @@ function construirTabla(xml) {
     var fila = document.createElement('tr');
     var imagenLocal = `<img class="logito" src='../imagenes/otras/logosequipos/${equipo}.png'>`;
     var claseEquipo = equipo.charAt(0).toLowerCase() + equipo.slice(1);
-    //fila.insertCell(0).innerHTML = imagenLocal;
-    //fila.insertCell(1).textContent = equipo;
-    var tdImagen = fila.insertCell(1);
-   tdImagen.innerHTML = imagenLocal;
-
-  // Agregar una clase al <td> correspondiente al equipo
-    tdImagen.classList.add(claseEquipo); 
+    fila.insertCell(0).innerHTML = imagenLocal;
     fila.insertCell(1).textContent = equipo;
     fila.insertCell(2).textContent = partidosJugados[equipo];
     fila.insertCell(3).textContent = victorias[equipo] || 0;
